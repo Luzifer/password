95e47e193fba3ef0e14cf0f27aa01534800eca46
Knut Ahlers <knut@ahlers.me>
2018-12-30 13:44:08 +0100

    Fix: RawGit is closing down, use Github URL
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- generateXKCDWordList.sh
+++ generateXKCDWordList.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
-set -eu
-set -o pipefail
+version="8744120d"
+source="https://raw.githubusercontent.com/leonardr/olipy/${version}/data/more-corpora/scribblenauts_words.txt"
 
-VERSION="8744120d"
-SOURCE="https://cdn.rawgit.com/leonardr/olipy/${VERSION}/data/more-corpora/scribblenauts_words.txt"
+words=$(curl -sL "${source}" | awk '/^[a-z]{4}[a-z]*$/{ print "\""$1"\"," }')
 
-WORDS=$(curl -sL "${SOURCE}" | awk '/^[a-z]{4}[a-z]*$/{ print "\""$1"\"," }')
-
-cat -s <<EOF > lib/xkcd_words.go
+cat -s <<EOF >lib/xkcd_words.go
 package securepassword
 
 // xkcdWordList contains a list of words derived from the scribblenauts
 // word list inside the olipy library by leonardr
 // https://github.com/leonardr/olipy
 var xkcdWordList = []string{
-  ${WORDS}
+  ${words}
 }
 EOF
 
